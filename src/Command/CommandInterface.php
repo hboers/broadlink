39fff5c288629d921bfb47c80628a626ee9b3446
Heinrich Boers <mail@heinrich-boers.net>
2020-07-08 19:14:08 +0200

    Update CommandInterface.php

--- src/Command/CommandInterface.php
+++ src/Command/CommandInterface.php
@@ -6,9 +6,9 @@ use TPG\Broadlink\Packet\Packet;
 
 interface CommandInterface
 {
-    public const COMMAND_DISCOVER=0x06;
-    public const COMMAND_AUTHENTICATE=0x65;
-    public const COMMAND_GET_INFO=0x6a;
+    const COMMAND_DISCOVER=0x06;
+    const COMMAND_AUTHENTICATE=0x65;
+    const COMMAND_GET_INFO=0x6a;
 
     public function getCommandId(): int ;
     public function handleResponse(Packet $packet);
